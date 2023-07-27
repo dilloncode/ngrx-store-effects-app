e15a29ec4e4a54b73b12c61f93fe77239dcc7db9
Mike <mikedillon@gmx.com>
2023-07-27 16:15:05 -0400

    state composition with selectors

--- src/products/store/reducers/pizzas.reducer.ts
+++ src/products/store/reducers/pizzas.reducer.ts
@@ -8,7 +8,16 @@ export interface PizzaState {
 }
 
 export const initialState: PizzaState = {
-  data: [],
+  data: [{
+    "name": "Plain Ol' Pepperoni",
+    "toppings": [
+      {
+        "id": 10,
+        "name": "pepperoni"
+      }
+    ],
+    "id": 3
+  }],
   loaded: false,
   loading: false,
 };
@@ -43,3 +52,7 @@ export function reducer(
 
   return state;
 }
+
+export const getPizzasLoading = (state: PizzaState) => state.loading;
+export const getPizzasLoaded = (state: PizzaState) => state.loaded;
+export const getPizzas = (state: PizzaState) => state.data;
