791712a5cd0e5369160b6cae2c5617d699d60bce
Mike <mikedillon@gmx.com>
2023-07-27 23:01:43 -0400

    router state and entity composition

--- src/products/store/reducers/index.ts
+++ src/products/store/reducers/index.ts
@@ -1,4 +1,4 @@
-import { ActionReducerMap, createFeatureSelector, createSelector } from '@ngrx/store';
+import { ActionReducerMap, createFeatureSelector } from '@ngrx/store';
 import * as fromPizzas from "./pizzas.reducer";
 
 export interface ProductsState {
@@ -10,14 +10,3 @@ export const reducers: ActionReducerMap<ProductsState> = {
 };
 
 export const getProducstState = createFeatureSelector<ProductsState>('products');
-
-// pizzas state
-export const getPizzaState = createSelector(
-  getProducstState,
-  (state: ProductsState) => state.pizzas
-);
-
-export const getPizzaEntities = createSelector(getPizzaState, fromPizzas.getPizzaEntities);
-export const getAllPizzas = createSelector(getPizzaEntities, (entities) => Object.keys(entities).map(id => entities[parseInt(id, 10)]))
-export const getPizzasLoaded = createSelector(getPizzaState, fromPizzas.getPizzasLoaded);
-export const getPizzasLoading = createSelector(getPizzaState, fromPizzas.getPizzasLoading);
---
+++ src/products/store/selectors/pizzas.selectors.ts
@@ -0,0 +1,33 @@
+import { createSelector } from '@ngrx/store';
+
+import * as fromRoot from '../../../app/store';
+import * as fromFeature from '../reducers';
+import * as fromPizzas from '../reducers/pizzas.reducer';
+
+import { Pizza } from '../../models/pizza.model';
+
+export const getPizzaState = createSelector(
+  fromFeature.getProducstState,
+  (state: fromFeature.ProductsState) => state.pizzas
+);
+
+export const getPizzaEntities = createSelector(
+  getPizzaState,
+  fromPizzas.getPizzaEntities
+);
+
+export const getSelectedPizza = createSelector(
+  getPizzaEntities,
+  fromRoot.getRouterState,
+  (entities, router): Pizza => {
+    return router.state && entities[router.state.params.pizzaId];
+  }
+);
+
+export const getAllPizzas = createSelector(
+  getPizzaEntities,
+  (entities) => Object.keys(entities).map(id => entities[parseInt(id, 10)])
+);
+
+export const getPizzasLoaded = createSelector(getPizzaState, fromPizzas.getPizzasLoaded);
+export const getPizzasLoading = createSelector(getPizzaState, fromPizzas.getPizzasLoading);
