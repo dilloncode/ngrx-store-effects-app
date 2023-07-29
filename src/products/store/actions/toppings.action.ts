4844f0e52013454b65de8096ade080f37a383353
Mike <mikedillon@gmx.com>
2023-07-28 21:31:32 -0400

    mapping ids to entities

--- src/products/store/actions/toppings.action.ts
+++ src/products/store/actions/toppings.action.ts
@@ -5,6 +5,7 @@ import { Topping } from '../../models/topping.model';
 export const LOAD_TOPPINGS = '[Products] Load Toppings';
 export const LOAD_TOPPINGS_FAIL = '[Products] Load Toppings Fail';
 export const LOAD_TOPPINGS_SUCCESS = '[Products] Load Toppings Success';
+export const VISUALIZE_TOPPINGS = '[Products] Visualize Toppings';
 
 export class LoadToppings implements Action {
   readonly type = LOAD_TOPPINGS;
@@ -12,13 +13,18 @@ export class LoadToppings implements Action {
 
 export class LoadToppingsFail implements Action {
   readonly type = LOAD_TOPPINGS_FAIL;
-  constructor(public payload: any) {};
+  constructor(public payload: any) { };
 }
 
 export class LoadToppingsSuccess implements Action {
   readonly type = LOAD_TOPPINGS_SUCCESS;
-  constructor(public payload: Topping[]) {};
+  constructor(public payload: Topping[]) { };
+}
+
+export class VisualizeToppings implements Action {
+  readonly type = VISUALIZE_TOPPINGS;
+  constructor(public payload: number[]) { }
 }
 
 // action types
-export type ToppingsAction = LoadToppings | LoadToppingsFail | LoadToppingsSuccess;
+export type ToppingsAction = LoadToppings | LoadToppingsFail | LoadToppingsSuccess | VisualizeToppings;
